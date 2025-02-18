8033f5427137ed015370c0b0af1123660d2eda5e
FilMarini <marinifil@gmail.com>
2025-02-18 11:41:56 +0100

    fixed lib name

--- ruckus.tcl
+++ ruckus.tcl
@@ -1,5 +1,5 @@
 # Load RUCKUS library
 source $::env(RUCKUS_PROC_TCL)
 
-loadSource -lib nilt -dir "$::DIR_PATH/rtl"
-loadSource -lib nilt -sim_only -dir "$::DIR_PATH/tb"
+loadSource -lib niltos -dir "$::DIR_PATH/rtl"
+loadSource -lib niltos -sim_only -dir "$::DIR_PATH/tb"
